4cfcb66bb044b2dae745c8f88c48247f49235ea9
glm <glm@opencode.ai>
2026-06-04 19:37:39 -1000

    gs_field_world.c: fn_80130BB0 100% via codex

--- src/game/gs_field_world.c
+++ src/game/gs_field_world.c
@@ -26093,7 +26093,7 @@ asm void fn_80130BB0(void) {
 #else
 void fn_80130BB0(u32 arg1) {
     extern u32 fn_8012A5B0(u8* a, u32 b, u32 c);
-    extern u32 fn_80135938(u32 a, u32 b);
+    extern u32 gamedataGetStatus(u32 a, u32 b);
     extern void fn_801240C4(u8* a, u32 b, u32 c, u32 d);
     extern void fn_80123D58(u8* a, u32 b, u32 c);
     extern u32 fn_80124410(u8* a, u32 b, s32 c, u32 d, u32 e);
@@ -26103,7 +26103,7 @@ void fn_80130BB0(u32 arg1) {
     u8 buf[0x140];
     u32 val, tmp;
     val = fn_8012A5B0((u8*)arg1, 2, 0);
-    fn_801240C4(buf, 0xc5, 0x1a, fn_80135938(0, 1));
+    fn_801240C4(buf, 0xc5, 0x1a, gamedataGetStatus(0, 1));
     fn_80123D58(buf, 0, 0x2c);
     fn_80123D58(buf, 1, 0x122);
     fn_80123D58(buf, 2, 0x10d);
