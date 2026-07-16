3ffa8b75e579fc20f4e13c3e3f4877ecc67a0842
dougchansan <dougchansan@users.noreply.github.com>
2026-07-16 01:03:55 -1000

    Link fight trainer AI damage unit

--- configure.py
+++ configure.py
@@ -1734,7 +1734,7 @@ config.libs = [
                 progress_category="game",
             ),
             Object(
-                CodeCandidate,
+                Matching,
                 "game/fight_trainer_ai_waza_damage.c",
                 mw_version="GC/1.3",
                 extra_cflags=["-O4,s", "-use_lmw_stmw on", "-sdata 8", "-sdata2 8"],
