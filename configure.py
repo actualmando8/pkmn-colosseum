b049baf93adad54495e75dac59a843922b8f31c6
dougchansan <dougchansan@users.noreply.github.com>
2026-07-16 10:10:00 -1000

    decomp: link kaisuu

--- configure.py
+++ configure.py
@@ -914,7 +914,7 @@ config.libs = [
                 progress_category="game",
             ),
             Object(
-                CodeCandidate,
+                Matching,
                 "game/kaisuu.c",
                 mw_version="GC/1.3",
                 extra_cflags=["-use_lmw_stmw on", "-sdata 8", "-sdata2 8"],
