6da5efc765bf5a3527627d1a39141f9778d4c779
dougchansan <dougchansan@users.noreply.github.com>
2026-07-16 08:00:56 -1000

    Match fn_80026740

--- src/game/menuNameEntry.c
+++ src/game/menuNameEntry.c
@@ -372,8 +372,9 @@ s32 fn_80026740(void* r3, u8* r4)
     s32  index;
     s32  x;
     f32  one;
-    f32  scale;
+    f32  new_var;
     void* ctx;
+    f32  scale;
     u8* entry;
 
     ctx = *(void**)((u8*)r3 + 0x60);
@@ -393,7 +394,9 @@ s32 fn_80026740(void* r3, u8* r4)
         x = index * 0x1a;
         x += *(s32*)(*(s32**)((u8*)ctx + 0x48));
         *(s16*)(r4 + 0x50) = (s16)x;
-        r4[0x67] = one - scale * *(f32*)(*(f32**)((u8*)ctx + 0x30));
+        new_var = *(f32*)(*(f32**)((u8*)ctx + 0x30));
+        new_var = one - (scale * new_var);
+        r4[0x67] = new_var;
     }
     return 0;
 }
