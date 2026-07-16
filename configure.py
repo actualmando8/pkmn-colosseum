68ee04d29c8fcccbd4fdf30ac5234168bcc3e095
dougchansan <dougchansan@users.noreply.github.com>
2026-07-16 01:08:56 -1000

    Match menuCB_Battle

--- configure.py
+++ configure.py
@@ -1578,7 +1578,7 @@ config.libs = [
                 progress_category="game",
             ),
             Object(
-                CodeCandidate,
+                Matching,
                 "game/menu/menuCB_Battle.c",
                 mw_version="GC/1.3",
                 extra_cflags=["-use_lmw_stmw on", "-sdata 8", "-sdata2 8"],
