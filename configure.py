7d47263ff83173651e94f2de1b45c8c5f989f7cb
dougchansan <dougchansan@users.noreply.github.com>
2026-07-18 19:42:16 -1000

    Link exact fight timer unit

--- configure.py
+++ configure.py
@@ -3889,7 +3889,7 @@ config.libs = [
                 progress_category="game",
             ),
             Object(
-                CodeCandidate,
+                Matching,
                 "game/fight_timer.c",
                 mw_version="GC/1.3",
                 extra_cflags=["-O4,s", "-use_lmw_stmw on", "-sdata 8", "-sdata2 8"],
