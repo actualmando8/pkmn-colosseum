ee48c3351496b6d0ec0bc6656307a5da8f1fdab5
dougchansan <dougchansan@users.noreply.github.com>
2026-07-21 13:52:06 -1000

    Match gamedata status unit

--- configure.py
+++ configure.py
@@ -687,7 +687,7 @@ config.libs = [
                 progress_category="game",
             ),
             Object(
-                CodeCandidate,
+                Matching,
                 "game/gamedata.c",
                 mw_version="GC/1.3",
                 extra_cflags=["-use_lmw_stmw on", "-sdata 8", "-sdata2 8"],
