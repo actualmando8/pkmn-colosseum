bf34a1b8167e3a5100fd58992749f104617bfea9
dougchansan <dougchansan@users.noreply.github.com>
2026-08-02 11:50:40 -1000

    Match Dolphin vector magnitude routines

--- src/dolphin/sdk_candidate_800A3A78.c
+++ src/dolphin/sdk_candidate_800A3A78.c
@@ -1,4 +1,6 @@
 /* Residual Dolphin matrix/vector suffix. */
 #define SDK_QUAT_SUFFIX_EXCLUDE
 #define SDK_CQUATSLERP_EXCLUDE
+#define SDK_MTXVEC_PREFIX_EXCLUDE
+#define SDK_PROJECTION_PREFIX_EXCLUDE
 #include "src/dolphin/sdk_range_800A37CC.c"
